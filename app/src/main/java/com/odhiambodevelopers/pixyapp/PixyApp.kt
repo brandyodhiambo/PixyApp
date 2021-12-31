8ea8b764f55b9fc67c53c78cdb6a254b1d34f8e1
brandy-kay <brandyariana99@gmail.com>
2021-12-31 08:20:30 +0300

    project Structure

---
+++ app/src/main/java/com/odhiambodevelopers/pixyapp/PixyApp.kt
@@ -0,0 +1,13 @@
+package com.odhiambodevelopers.pixyapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class PixyApp:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
---
+++ app/src/main/java/com/odhiambodevelopers/pixyapp/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.odhiambodevelopers.pixyapp.utils
+
+object Constants {
+    const val KEY = "24824065-c0e44707295f50e1e3071e213"
+    const val BASE_URL = "https://pixabay.com/api/"
+}
